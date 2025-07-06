f4801d9950b8657ce2e69e154f7188273ba37137
Mochammad Irsyad Kurniawan <mochammadirsyadkurniawan@Mochammads-MacBook-Air.local>
2025-07-06 19:33:08 +0700

    update baru untuk benerin erro

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,17 @@
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: 'resources/js/app.jsx',
+//             refresh: true,
+//         }),
+//         react(),
+//     ],
+// });
+
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
@@ -5,7 +19,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: [
+                'resources/js/app.jsx',
+                'resources/css/swiper-custom.css',
+                'resources/css/user/styles.css',
+                'resources/css/workout.css',
+                'resources/css/app.css',
+                'resources/css/chatbot.css',
+            ],
             refresh: true,
         }),
         react(),
